e0a7a64640d6590cc49d1091084edf0151ba8ade
Laszlo Almasy <laszlo.almasydezsadany@gmail.com>
2019-03-20 12:40:02 +0100

    view command line

--- phase/view.py
+++ phase/view.py
@@ -2,12 +2,31 @@ import matplotlib.pyplot as plt
 import cv2
 import os
 import numpy as np
+import argparse
 
 # Root directory of the project
 ROOT_DIR = os.path.abspath("../")
 
-mask = os.path.join(ROOT_DIR, "results/20190314_112705_merge/exp0249.png")
-phase = os.path.join(ROOT_DIR, "test_data/exp0249.jpg")
+# Parse command line arguments
+parser = argparse.ArgumentParser(
+    description='Visualisation of an image and its mask')
+parser.add_argument('--img', required=True,
+                    metavar="/path/to/image.*",
+                    help='Path to image')
+parser.add_argument('--mask', required=True,
+                    metavar="/path/to/mask.*",
+                    help="Path to mask")
+args = parser.parse_args()
+
+mask = os.path.join(ROOT_DIR, args.mask)
+phase = os.path.join(ROOT_DIR, args.img)
+
+if not os.path.isfile(mask):
+    print('Invalid mask path:', mask)
+    exit()
+if not os.path.isfile(phase):
+    print('Invalid image path:', phase)
+    exit()
 
 phase = cv2.imread(phase, 0)
 mask = cv2.imread(mask, 0)
